a96ad35d348a2ed8554dadda59a8f18688811eb3
kaczuchg711 <kaczatom4@gmail.com>
2019-03-16 20:18:29 +0100

    done

--- zadanie.pl
+++ zadanie.pl
@@ -1,5 +1,19 @@
+dopasuj(0,[G|_],G) :- !.
+dopasuj(Q,[_|C],X) :- Q1 is Q - 1, dopasuj(Q1,C,X).   
+
 mysl(J,F,S,A,Sz,R,M) :-
+    
+    Pies=0,
+    Rybka=1,
+    Kanarek=2,
+    Papuga=3,
+    Kot=4,
+    Chomik=5,
+    Waz=6,
+
     Z0 = ["pies","kot","rybka","chomik","papuga","waz","kanarek"],
+    P0 = [0,1,2,3,4,5,6],
+
     J=[PJ,ZJ],
     F=[PF,ZF],
     S=[PS,ZS],
@@ -7,10 +21,19 @@ mysl(J,F,S,A,Sz,R,M) :-
     Sz=[PSz,ZSz],
     R=[PR,ZR],
     M=[PM,ZM],
-    select(ZJ,Z0,Z1),ZJ\=chomik,ZJ\=rybka,ZJ\=papuga,ZJ\=kanarek,ZJ\=kot,ZJ\=waz,
-    select(ZF,Z1,Z2),ZF\=papuga,ZF\=waz,ZF\=chomik,
-    select(ZS,Z2,Z3),ZS\=kanarek,ZS\=chomik,ZS\=papuga,
-    select(ZA,Z3,Z4),ZA\=waz,ZA\=kot,ZA\=chomik,ZA\=rybka,ZA\=psa,ZA\=papuga,
-    select(ZSz,Z4,Z5),ZSz\=kot,ZSz\=kanarek,
-    select(ZR,Z5,Z6),ZR\=waz,ZR\=rybka,ZR\=papuga,ZR\=chomik,
-    select(ZM,Z6,Z7),ZM\=kanarek,ZM\=kot,ZM\=waz,ZM\=papuga.
+    
+    select(PJ,P0,P1),   PJ\=Chomik, PJ\=Rybka,      PJ\=Papuga, PJ\=Kanarek,    PJ\=Kot,    PJ\=Waz,    
+    select(PF,P1,P2),   PF\=Papuga, PF\=Waz,        PF\=Chomik,                 PJ<PF,
+    select(PS,P2,P3),   PS\=Kanarek,PS\=Chomik,     PS\=Papuga,                 PJ<PS,      PF<PS,
+    select(PA,P3,P4),   PA\=Waz,    PA\=Kot,        PA\=Chomik, PA\=Rybka,      PA\=Pies,   PA\=Papuga, PF<PA,  PA>PJ,  PA<PS,
+    select(PSz,P4,P5),  PSz\=Kot,   PSz\=Kanarek,                               PSz>PA,     PSz>PF,     PS>PSz, PSz>PJ,
+    select(PR,P5,P6),   PR\=Waz,    PR\=Rybka,      PR\=Papuga, PR\=Chomik,     PJ<PR,      PF<PR,      PSz<PR, PR>PA,  PR<PS,
+    select(PM,P6,_),    PM\=Kanarek,PM\=Kot,        PM\=Waz,    PM\=Papuga,     PS>PM,      PA<PM,      PM>PR,  PM>PF,  PM>PJ,  PM>PSz,
+
+    dopasuj(PJ,Z0,ZJ),
+    dopasuj(PF,Z0,ZF),
+    dopasuj(PS,Z0,ZS),
+    dopasuj(PA,Z0,ZA),
+    dopasuj(PSz,Z0,ZSz),
+    dopasuj(PR,Z0,ZR),
+    dopasuj(PM,Z0,ZM).
